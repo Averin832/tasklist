26846367d75b7e950cc3f7af2fde577edc6f7ca4
Reihold <Reihold@192.168.1.5>
2022-03-07 21:45:09 +0900

    Index/IndexServlet/app.jspの作成

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -16,6 +16,10 @@ import javax.persistence.Table;
     @NamedQuery(
             name = "getAllTasks",
             query = "select m from Task as m order by m.id desc"
+            ),
+    @NamedQuery(
+            name  = "getTasksCount",
+            query = "select count(t) from Task as t"
             )
 })
 @Table(name = "tasks")
